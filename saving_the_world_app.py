53426ea77e25c7887cdf16596b0652056ab88135
pgunter25 <37387761+pgunter25@users.noreply.github.com>
2022-08-29 15:27:49 +1000

    add delimeter for csv

--- saving_the_world_app.py
+++ saving_the_world_app.py
@@ -14,7 +14,7 @@ from pathlib import Path
 #Function to get the cities data 
 def get_cities():
     url = 'https://github.com/Saving-the-world-org/saving_the_world_app/blob/a1d8e00dcd1ac6aeb50d8dc14601847e382abe5f/Data/Cities%20-%20Cities.csv'
-    cities_df = pd.read_csv(url, index_col=0)
+    cities_df = pd.read_csv(url, delimiter="," index_col=0)
     #data = Path("Data/Cities - Cities.csv")
     #cities_df = pd.read_csv(data, delimiter=",").rename(columns={"Unnamed: 0":"Instance"})
     return cities_df
@@ -22,7 +22,7 @@ def get_cities():
 #Function to return the donor_df
 def get_donors():
     url = 'https://github.com/Saving-the-world-org/saving_the_world_app/blob/3480cc3cfcfb14f904130666e1d8989ad148d2e7/Data/Donations%20-%20Donations.csv'
-    donor_df = pd.read_csv(url, index_col=0)
+    donor_df = pd.read_csv(url, delimiter="," index_col=0)
     #data = Path("/Users/phoebegunter/Documents/FinTech-Workspace/project3/Data/Donations - Donations.csv")
     #donor_df = pd.read_csv(data, delimiter=",").rename(columns={"Unnamed: 0":"Instance"})
     return donor_df
@@ -30,7 +30,7 @@ def get_donors():
 #Function to return the org_df
 def get_orgs():
     url = 'https://github.com/Saving-the-world-org/saving_the_world_app/blob/3480cc3cfcfb14f904130666e1d8989ad148d2e7/Data/Organizations%20-%20Organizations.csv'
-    org_df = pd.read_csv(url, index_col=0)
+    org_df = pd.read_csv(url, delimiter="," index_col=0)
     # data = Path("/Users/phoebegunter/Documents/FinTech-Workspace/project3/Data/Organizations - Organizations.csv")
     # org_df = pd.read_csv(data, delimiter=",").rename(columns={"Unnamed: 0":"Instance"})
     return org_df
